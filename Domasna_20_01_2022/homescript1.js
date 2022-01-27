19295792ef8964317439dd4701af626b1d2077f8
cvetanjoseski <cvetanjoseski@gmail.com>
2022-01-27 17:08:39 +0100

    homework

--- Domasna_20_01_2022/homescript1.js
+++ Domasna_20_01_2022/homescript1.js
@@ -3,17 +3,13 @@
   .then(response => response.json())
   .then(object => {  document.getElementById("jsonCall").addEventListener("click",function(){
     let myPage = document.getElementById("firstDiv");
-    let newH4 ;
-    newH4 = document.createElement('h4',);
           console.log(object);
-         for(let i in object){
-            if(object[i].completed==true){      
-               newH4.append(`${object[i].id} ${object[i].title}-----Completed `);
-               myPage.appendChild(newH4);     
+         for(let i of object){
+            if(i.completed==true){      
+              myPage.innerHTML += `<div>${i.id} ${i.title}-----Completed </div>`;     
             }
             else{               
-              newH4.append(`${object[i].id} ${object[i].title}-----Not completed `); 
-               myPage.appendChild(newH4);
+              myPage.innerHTML += `<div>${i.id} ${i.title}-----Not completed </div>`;
             }
          }
       
